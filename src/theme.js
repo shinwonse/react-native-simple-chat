8bfbda84bbcfcc9fa169f42f8bb35fc2042853b5
wonse <sinwonse@gmail.com>
2021-09-24 11:31:33 +0900

    login

--- src/theme.js
+++ src/theme.js
@@ -10,4 +10,9 @@ const colors = {
 export const theme = {
     background: colors.white,
     text: colors.black,
+
+    imageBackground: colors.grey_0,
+    label: colors.grey_1,
+    inputPlaceholder: colors.grey_1,
+    inputBorder: colors.grey_1,
 };
