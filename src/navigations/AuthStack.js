237c9afcc57f0f9a4ae9c8a3712a7c345844a947
wonse <sinwonse@gmail.com>
2021-09-25 12:01:55 +0900

    login update

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -13,10 +13,11 @@ const AuthStack = () => {
             screenOptions={{
                 headerTitleAlign: 'center',
                 cardStyle: { backgroundColor: theme.backgroundColor },
+                headerTintColor: theme.headerTintColor,
             }}
         >
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Signup" component={Signup} />
+            <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+            <Stack.Screen name="Signup" component={Signup} options={{headerBackTitleVisible: false}}/>
         </Stack.Navigator>
     );
 };
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
-import { Text, Button }  from 'react-native';
-import { Image, Input } from '../components';
+import { TouchableWithoutFeedback, Keyboard }  from 'react-native';
+import { Image, Input, Button } from '../components';
 import { images } from '../utils/images';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { validateEmail, removeWhitespace } from '../utils/common';
 
 const Container = styled.View`
     flex: 1;
@@ -12,33 +14,72 @@ const Container = styled.View`
     padding: 20px;
 `;
 
+const ErrorText = styled.Text`
+    align-items: flex-start;
+    width: 100%;
+    height: 20px;
+    margin-bottom: 10px;
+    line-height: 20px;
+    color: ${({theme})=>theme.errorText};
+`;
+
 const Login = ({navigation}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [disabled, setDisabled] = useState(true);
     const passwordRef = useRef();
 
+    useEffect(() => {
+        setDisabled(!(email && password && !errorMessage));
+    }, [email, password, errorMessage]);
+
+    const _handleEmailChange = email => {
+        const changedEmail = removeWhitespace(email);
+        setEmail(changedEmail);
+        setErrorMessage(
+            validateEmail(changedEmail) ? ' ' : 'Please verify your email.'
+        );
+    };
+    const _handlePasswordChange = password => {
+        setPassword(removeWhitespace(password));
+    };
+    const _handleLoginButtonPreess = () => {};
+
     return (
-        <Container>
-            <Image url={images.logo} imageStyle={{ borderRadius: 8}} />
-            <Input 
-                label="Email"
-                value={email}
-                onChangeText={text => setEmail(text)}
-                onSubmitEditing={() => passwordRef.current.focus()}
-                placeholder="Email"
-                returnKeyType="next"
-            />
-            <Input
-                ref={passwordRef} 
-                label="Password"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                onSubmitEditing={() => {}}
-                placeholder="Password"
-                returnKeyType="done"
-                isPassword
-            />
-        </Container>
+        <KeyboardAwareScrollView
+            contentContainerStyle={{ flex: 1 }}
+            extraScrollHeight={20}
+        >
+            <Container>
+                <Image url={images.logo} imageStyle={{ borderRadius: 8}} />
+                <Input 
+                    label="Email"
+                    value={email}
+                    onChangeText={_handleEmailChange}
+                    onSubmitEditing={_handleLoginButtonPreess}
+                    placeholder="Email"
+                    returnKeyType="next"
+                />
+                <Input
+                    ref={passwordRef} 
+                    label="Password"
+                    value={password}
+                    onChangeText={_handlePasswordChange}
+                    onSubmitEditing={() => {}}
+                    placeholder="Password"
+                    returnKeyType="done"
+                    isPassword
+                />
+                <ErrorText>{errorMessage}</ErrorText>
+                <Button title="Login" onPress={_handleLoginButtonPreess} disabled={disabled}/>
+                <Button 
+                    title="Sign up with email"
+                    onPress={() => navigation.navigate('Signup')}
+                    isFilled={false}
+                />
+            </Container>
+        </KeyboardAwareScrollView>       
     );
 };
 
